8914402dbcbe052c53017a43dc75594c578ed2b2
Rodrigo <rodrigo.bento2010@gmail.com>
2025-05-26 10:43:58 -0300

    Parte de interfaces completa

---
+++ default_methods/src/entities/USAInterestService.java
@@ -0,0 +1,18 @@
+package entities;
+
+import java.security.InvalidParameterException;
+
+public class USAInterestService implements InterestService {
+    private double interestRate;
+
+    public USAInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+
+}
